b59c372e5ec305936fb8b9996dbd82563489c113
NotKeira <keirahopkins316@gmail.com>
2024-12-26 11:22:47 +0100

    changed projects data

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,19 +1,27 @@
 const projects = [
   {
     id: "portfolio",
-    title: "My Portfolio",
+    title: "Portfolio",
     description:
       "This is my portfolio, it was built using NextJS and uses TypeScript.",
     technologies: ["React", "Node.js", "NextJS", "TypeScript"],
     link: "https://github.com/NotKeira/portfolio",
   },
   {
-    id: "pylog",
-    title: "PyLog",
+    id: "robodactyl",
+    title: "RoboDactyl",
     description:
-      "PyLog is a simple logging library for Python, it's easy to use and has a lot of features.",
-    technologies: ["Python 3"],
-    link: "https://github.com/NotKeira/pylog",
+      "RoboDactyl is a Discord bot that I made for the sole purpose of running things inside a pterodactyl minecraft server via discord.",
+    technologies: ["Java", "Pterodactyl API", "Discord API", "Minecraft", "JDA"],
+    link: "https://github.com/NotKeira/RoboDactyl",
+  },
+  {
+    id: "topggcli",
+    title: "Top.gg CLI",
+    description:
+      "A CLI tool to interact with the top.gg API. It can be used to get information about bots, users, and more.",
+    technologies: ["Rust", "Top.gg API", "CLI"],
+    link: "https://github.com/NotKeira/topgg-cli",
   },
 ];
 export default projects;
